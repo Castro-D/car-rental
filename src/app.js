a3357844a0dfd206cde858a91dc7299369bb6875
Castro-D <castro.dh91@gmail.com>
2021-08-23 15:05:24 -0300

    get all data from db
    then log it in console to verify that its ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const Database = require('better-sqlite3');
+const { fromDbToEntity } = require('./mapper/carMapper');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const db = new Database('./data/database.db', { verbose: console.log });
+
+function getAllCars() {
+  const cars = db.prepare(
+    `SELECT
+      id,
+      make,
+      model,
+      year,
+      kms,
+      color,
+      air_conditioning,
+      number_passengers,
+      transmission,
+      car_img_url
+
+    FROM cars`,
+  )
+    .all();
+  return cars.map((carData) => fromDbToEntity(carData));
+}
+
+async function index(req, res) {
+  const data = await getAllCars();
+  res.send(data);
+}
+
+app.get('/', index);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- src/config/setup.sql
+++ src/config/setup.sql
@@ -1,12 +1,14 @@
 CREATE TABLE cars (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   make TEXT NOT NULL,
   model TEXT NOT NULL,
-  year TEXT NOT NULL,
-  kms TEXT NOT NULL,
+  year INTEGER NOT NULL,
+  kms INTEGER NOT NULL,
   color TEXT NOT NULL,
   air_conditioning TEXT NOT NULL,
-  number_passengers TEXT NOT NULL,
+  number_passengers INTEGER NOT NULL,
   transmission TEXT NOT NULL,
-  car_img_url TEXT NOT NULL
+  car_img_url TEXT NOT NULL,
+  created_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
+  updated_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL
 );
