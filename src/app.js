a113e62645926514be8cc959cadf10a68a6a4ddb
Castro-D <castro.dh91@gmail.com>
2021-09-10 19:26:29 -0300

    add repository module

--- src/app.js
+++ src/app.js
@@ -29,60 +29,15 @@ nunjucks.configure('src/module', {
   express: app,
 });
 
-async function getAllCars() {
-  const cars = db.prepare(
-    `SELECT
-      id,
-      make,
-      model,
-      year,
-      kms,
-      color,
-      air_conditioning,
-      number_passengers,
-      transmission,
-      car_img_url
-
-    FROM cars`,
-  )
-    .all();
-  return cars.map((carData) => fromDbToEntity(carData));
-}
-
 async function index(req, res) {
   const cars = await getAllCars();
   res.render('car/view/home.html', { cars });
-}
-
-async function getCarbyId(id) {
-  const car = db.prepare(
-    `SELECT
-    id,
-    make,
-    model,
-    year,
-    kms,
-    color,
-    air_conditioning,
-    number_passengers,
-    transmission, 
-    car_img_url
-    FROM cars WHERE id = ?`,
-  ).get(id);
-  return fromDbToEntity(car);
 }
 
 async function view(req, res) {
   const { id } = req.params;
   const car = await getCarbyId(id);
   res.render('car/view/car-info.html', { car });
-}
-
-function deleteCar(car) {
-  db.prepare(
-    `DELETE FROM cars WHERE id = ?
-    `,
-  ).run(car.id);
 }
 
 app.get('/', index);
