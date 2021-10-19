7ebe079c4466b5f919bd5145b8f2bb099888977e
Castro-D <castro.dh91@gmail.com>
2021-10-19 18:18:50 -0300

    add tests for controller exceptions

--- src/module/car/controller/__test__/car.test.js
+++ src/module/car/controller/__test__/car.test.js
@@ -12,6 +12,10 @@ const serviceMock = {
   getAllCars: jest.fn(() => Promise.resolve([]))
 }
 
+global.console = {
+  log: jest.fn()
+}
+
 const controller = new CarController(upploadMiddleware, serviceMock);
 
 test('index renders index.html', async () => {
@@ -71,4 +75,76 @@ test('save creates a car when id is not given', async () => {
   expect(serviceMock.save).toHaveBeenCalledWith(bodyMock);
   expect(redirectMock).toHaveBeenCalledTimes(1);
   expect(redirectMock).toHaveBeenCalledWith('/');
+});
+
+test('delete method calls service with id', async () => {
+  const FAKE_CAR = new Car({ id: 1 });
+  serviceMock.getCarById.mockImplementationOnce(() => Promise.resolve(FAKE_CAR));
+  const redirectMock = jest.fn();
+
+  await controller.delete({ params: { id: 1 } }, { redirect: redirectMock });
+
+  expect(serviceMock.deleteCar).toHaveBeenCalledTimes(1);
+  expect(serviceMock.deleteCar).toHaveBeenCalledWith(FAKE_CAR);
+  expect(redirectMock).toHaveBeenCalledTimes(1);
+  expect(redirectMock).toHaveBeenLastCalledWith('/');
+});
+
+test('configure routes properly', async () => {
+  const app = {
+    get: jest.fn(),
+    post: jest.fn()
+  }
+  controller.configureRoutes(app);
+});
+
+test('view renders car', async () => {
+  const MOCK_ID = 1;
+  const renderMock = jest.fn();
+  await controller.view({ params: { id: MOCK_ID } }, { render: renderMock });
+
+  expect(serviceMock.getCarById).toHaveBeenCalledWith(MOCK_ID);
+  expect(renderMock).toHaveBeenCalledWith('car/view/car-info.html', { car: {} })
+});
+
+test('view displays error on exception', async () => {
+  serviceMock.getCarById.mockImplementationOnce(() => { throw Error('testing') });
+  await controller.view({ params: { id: 1 }}, {});
+
+  expect(global.console.log).toHaveBeenCalledTimes(1);
+});
+
+test('save displays error on exception', async () => {
+  global.console.log.mockReset();
+  const bodyMock = new Car({
+    make: undefined,
+    model: undefined,
+    year: undefined,
+    kms: undefined,
+    color: undefined,
+    airConditioning: undefined,
+    numberPassengers: undefined,
+    transmission: undefined,
+    carImgUrl: 'example/car.jpg',
+  });
+  const redirectMock = jest.fn();
+  serviceMock.save.mockImplementationOnce(() => { throw Error('testing') });
+  await controller.save({ body: bodyMock, file: { path: 'example/car.jpg' } }, { redirect: redirectMock });
+
+  expect(global.console.log).toHaveBeenCalledTimes(1);
+});
+
+test('delete displays error on exception', async () => {
+  global.console.log.mockReset();
+  const redirectMock = jest.fn();
+  serviceMock.deleteCar.mockImplementationOnce(() => { throw Error('testing') });
+  await controller.delete({ params: { id: 1 }}, { redirect: redirectMock });
+  expect(global.console.log).toHaveBeenCalledTimes(1);
+});
+
+test('index displays error on exception', async () => {
+  global.console.log.mockReset();
+  serviceMock.getAllCars.mockImplementationOnce(() => { throw Error('tesing') });
+  await controller.index({}, {});
+  expect(global.console.log).toHaveBeenCalledTimes(1);
 });
